6947c7d08896a4166619ef903f66f119a4609838
Als <malkautsars@gmail.com>
2020-04-15 01:56:01 +0800

    feat: new POST login

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static("views"))
 
+// body-parser - read HTTP POST data from Form Input.
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
 // Routing
 const indexView = require('./routes/index')
 const panelView = require('./routes/panel')
--- routes/panel.js
+++ routes/panel.js
@@ -2,7 +2,22 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.render('panel/index')
+    res.render('panel/index', {
+        userName: '',
+        userPass: ''
+    })
+})
+
+app.post('/login', (req, res) => {
+    let username = req.body.userName;
+    let pass = req.body.userPass;
+    if (username === "admin" && pass === "admin") {
+        res.redirect('/panel/dashboard')
+    }
+})
+
+app.get('/dashboard', (req, res) => {
+    res.render('panel/dashboard')
 })
 
 module.exports = app
