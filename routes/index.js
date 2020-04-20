af56b9261c29b749e58bca67e0add50b85ccbd86
Als <malkautsars@gmail.com>
2020-04-20 21:59:07 +0800

    feat: init blog page

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/blog', (req, res) => {
+    res.render('blog')
+})
+
+app.get('/blog-detail', (req, res) => {
+    res.render('blog-detail')
+})
+
 module.exports = app
