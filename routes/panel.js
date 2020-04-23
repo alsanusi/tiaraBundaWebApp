696df60ad662922f28e8b558f6d53f90e4bedfc5
Als <malkautsars@gmail.com>
2020-04-23 14:13:49 +0800

    fix: restructure one endpoint

--- routes/panel.js
+++ routes/panel.js
@@ -96,37 +96,20 @@ app.get('/dashboard', redirectLogin, (req, res) => {
 })
 
 // Siswa
-app.get('/tambahDataSiswa', redirectLogin, (req, res) => {
-    res.render('panel/admin/siswa/tambahDataSiswa', {
-        id: generateIdSiswa(),
-        kelas: 1,
-        status: 'Siswa',
-        semester: 1
-    })
-})
-
 const fotoProfilSiswa = upload.single('fotoProfil')
-app.post('/tambahDataSiswa', redirectLogin, (req, res) => {
-    fotoProfilSiswa(req, res, (err) => {
-        if (err) {
-            let error_msg = "Besar foto profil siswa melebihi 3 MB!"
-            req.flash('error', error_msg)
-            res.render('panel/admin/siswa/tambahDataSiswa', {
-                id: generateIdSiswa(),
-                namaLengkap: '',
-                tempatLahir: '',
-                tanggalLahir: '',
-                kelas: 1,
-                semester: 1,
-                alamat: '',
-                namaAyah: '',
-                namaIbu: '',
-                nomorTelefon: '',
-                status: 'Siswa'
-            })
-        } else {
-            if (req.file === null) {
-                let error_msg = 'Input foto profil siswa!'
+app.route('/tambahDataSIswa', redirectLogin)
+    .get((req, res) => {
+        res.render('panel/admin/siswa/tambahDataSiswa', {
+            id: generateIdSiswa(),
+            kelas: 1,
+            status: 'Siswa',
+            semester: 1
+        })
+    })
+    .post((req, res) => {
+        fotoProfilSiswa(req, res, (err) => {
+            if (err) {
+                let error_msg = "Besar foto profil siswa melebihi 3 MB!"
                 req.flash('error', error_msg)
                 res.render('panel/admin/siswa/tambahDataSiswa', {
                     id: generateIdSiswa(),
@@ -142,57 +125,74 @@ app.post('/tambahDataSiswa', redirectLogin, (req, res) => {
                     status: 'Siswa'
                 })
             } else {
-                let dataSiswa = {
-                    id: generateIdSiswa(),
-                    fotoProfil: req.file.filename,
-                    namaLengkap: req.sanitize('namaLengkap').escape().trim(),
-                    tempatLahir: req.sanitize('tempatLahir').escape().trim(),
-                    tanggalLahir: req.sanitize('tanggalLahir').escape().trim(),
-                    kelas: 1,
-                    semester: 1,
-                    alamat: req.sanitize('alamat').escape().trim(),
-                    namaAyah: req.sanitize('namaAyah').escape().trim(),
-                    namaIbu: req.sanitize('namaIbu').escape().trim(),
-                    nomorTelefon: req.sanitize('nomorTelefon').escape().trim(),
-                    status: 'Siswa'
-                }
-                dbConnection.con.query('INSERT INTO dataSiswa SET ?', dataSiswa, (err, result) => {
-                    if (err) {
-                        req.flash('error', err)
-                        res.render('panel/admin/siswa/tambahDataSiswa', {
-                            id: dataSiswa.id,
-                            namaLengkap: dataSiswa.namaLengkap,
-                            tempatLahir: dataSiswa.tempatLahir,
-                            tanggalLahir: dataSiswa.tanggalLahir,
-                            kelas: dataSiswa.kelas,
-                            semester: dataSiswa.semester,
-                            alamat: dataSiswa.alamat,
-                            namaAyah: dataSiswa.namaAyah,
-                            namaIbu: dataSiswa.namaIbu,
-                            nomorTelefon: dataSiswa.nomorTelefon,
-                            status: dataSiswa.status
-                        })
-                    } else {
-                        req.flash('success', 'Data siswa berhasil ditambahkan!')
-                        res.render('panel/admin/siswa/tambahDataSiswa', {
-                            id: generateIdSiswa(),
-                            namaLengkap: '',
-                            tempatLahir: '',
-                            tanggalLahir: '',
-                            kelas: 1,
-                            semester: 1,
-                            alamat: '',
-                            namaAyah: '',
-                            namaIbu: '',
-                            nomorTelefon: '',
-                            status: 'Siswa'
-                        })
+                if (req.file === null) {
+                    let error_msg = 'Input foto profil siswa!'
+                    req.flash('error', error_msg)
+                    res.render('panel/admin/siswa/tambahDataSiswa', {
+                        id: generateIdSiswa(),
+                        namaLengkap: '',
+                        tempatLahir: '',
+                        tanggalLahir: '',
+                        kelas: 1,
+                        semester: 1,
+                        alamat: '',
+                        namaAyah: '',
+                        namaIbu: '',
+                        nomorTelefon: '',
+                        status: 'Siswa'
+                    })
+                } else {
+                    let dataSiswa = {
+                        id: generateIdSiswa(),
+                        fotoProfil: req.file.filename,
+                        namaLengkap: req.sanitize('namaLengkap').escape().trim(),
+                        tempatLahir: req.sanitize('tempatLahir').escape().trim(),
+                        tanggalLahir: req.sanitize('tanggalLahir').escape().trim(),
+                        kelas: 1,
+                        semester: 1,
+                        alamat: req.sanitize('alamat').escape().trim(),
+                        namaAyah: req.sanitize('namaAyah').escape().trim(),
+                        namaIbu: req.sanitize('namaIbu').escape().trim(),
+                        nomorTelefon: req.sanitize('nomorTelefon').escape().trim(),
+                        status: 'Siswa'
                     }
-                })
+                    dbConnection.con.query('INSERT INTO dataSiswa SET ?', dataSiswa, (err, result) => {
+                        if (err) {
+                            req.flash('error', err)
+                            res.render('panel/admin/siswa/tambahDataSiswa', {
+                                id: dataSiswa.id,
+                                namaLengkap: dataSiswa.namaLengkap,
+                                tempatLahir: dataSiswa.tempatLahir,
+                                tanggalLahir: dataSiswa.tanggalLahir,
+                                kelas: dataSiswa.kelas,
+                                semester: dataSiswa.semester,
+                                alamat: dataSiswa.alamat,
+                                namaAyah: dataSiswa.namaAyah,
+                                namaIbu: dataSiswa.namaIbu,
+                                nomorTelefon: dataSiswa.nomorTelefon,
+                                status: dataSiswa.status
+                            })
+                        } else {
+                            req.flash('success', 'Data siswa berhasil ditambahkan!')
+                            res.render('panel/admin/siswa/tambahDataSiswa', {
+                                id: generateIdSiswa(),
+                                namaLengkap: '',
+                                tempatLahir: '',
+                                tanggalLahir: '',
+                                kelas: 1,
+                                semester: 1,
+                                alamat: '',
+                                namaAyah: '',
+                                namaIbu: '',
+                                nomorTelefon: '',
+                                status: 'Siswa'
+                            })
+                        }
+                    })
+                }
             }
-        }
+        })
     })
-})
 
 app.post('/cariDataSiswa', redirectLogin, async (req, res) => {
     let inputKelas = req.body.kelas;
@@ -223,27 +223,17 @@ app.get('/kelolaDataGuru', redirectLogin, (req, res) => {
 })
 
 // Berita
-app.get('/tambahDataBerita', redirectLogin, (req, res) => {
-    res.render('panel/admin/berita/tambahDataBerita', {
-        penulis: adminCredential.username
-    })
-})
-
 const gambarBerita = upload.single('gambarBerita')
-app.post('/tambahDataBerita', redirectLogin, (req, res) => {
-    gambarBerita(req, res, (err) => {
-        if (err) {
-            let error_msg = "Besar gambar berita melebihi 3 MB!"
-            req.flash('error', error_msg)
-            res.render('panel/admin/berita/tambahDataBerita', {
-                judulBerita: '',
-                tanggalUpdate: '',
-                penulis: adminCredential.username,
-                deskripsiBerita: ''
-            })
-        } else {
-            if (req.file === null) {
-                let error_msg = "Input gambar berita!"
+app.route('/tambahDataBerita', redirectLogin)
+    .get((req, res) => {
+        res.render('panel/admin/berita/tambahDataBerita', {
+            penulis: adminCredential.username
+        })
+    })
+    .post((req, res) => {
+        gambarBerita(req, res, (err) => {
+            if (err) {
+                let error_msg = "Besar gambar berita melebihi 3 MB!"
                 req.flash('error', error_msg)
                 res.render('panel/admin/berita/tambahDataBerita', {
                     judulBerita: '',
@@ -252,37 +242,47 @@ app.post('/tambahDataBerita', redirectLogin, (req, res) => {
                     deskripsiBerita: ''
                 })
             } else {
-                let dataBerita = {
-                    gambarBerita: req.file.filename,
-                    judulBerita: req.sanitize("judulBerita").escape().trim(),
-                    tanggalUpdate: req.sanitize("tanggalUpdate").escape().trim(),
-                    deskripsi: req.sanitize("deskripsiBerita").escape().trim(),
-                    penulis: adminCredential.username
-                }
-                dbConnection.con.query("INSERT INTO dataBerita SET ?", dataBerita, (err, result) => {
-                    if (err) {
-                        req.flash('error', err)
-                        res.render('panel/admin/berita/tambahDataBerita', {
-                            judulBerita: dataBerita.gambarBerita,
-                            tanggalUpdate: dataBerita.tanggalUpdate,
-                            penulis: adminCredential.username,
-                            deskripsiBerita: dataBerita.deskripsi
-                        })
-                    } else {
-                        req.flash('success', "Berita berhasil ditambakan!")
-                        res.render('panel/admin/berita/tambahDataBerita', {
-                            gambarBerita: '',
-                            judulBerita: '',
-                            tanggalUpdate: '',
-                            penulis: adminCredential.username,
-                            deskripsiBerita: ''
-                        })
+                if (req.file === null) {
+                    let error_msg = "Input gambar berita!"
+                    req.flash('error', error_msg)
+                    res.render('panel/admin/berita/tambahDataBerita', {
+                        judulBerita: '',
+                        tanggalUpdate: '',
+                        penulis: adminCredential.username,
+                        deskripsiBerita: ''
+                    })
+                } else {
+                    let dataBerita = {
+                        gambarBerita: req.file.filename,
+                        judulBerita: req.sanitize("judulBerita").escape().trim(),
+                        tanggalUpdate: req.sanitize("tanggalUpdate").escape().trim(),
+                        deskripsi: req.sanitize("deskripsiBerita").escape().trim(),
+                        penulis: adminCredential.username
                     }
-                })
+                    dbConnection.con.query("INSERT INTO dataBerita SET ?", dataBerita, (err, result) => {
+                        if (err) {
+                            req.flash('error', err)
+                            res.render('panel/admin/berita/tambahDataBerita', {
+                                judulBerita: dataBerita.gambarBerita,
+                                tanggalUpdate: dataBerita.tanggalUpdate,
+                                penulis: adminCredential.username,
+                                deskripsiBerita: dataBerita.deskripsi
+                            })
+                        } else {
+                            req.flash('success', "Berita berhasil ditambakan!")
+                            res.render('panel/admin/berita/tambahDataBerita', {
+                                gambarBerita: '',
+                                judulBerita: '',
+                                tanggalUpdate: '',
+                                penulis: adminCredential.username,
+                                deskripsiBerita: ''
+                            })
+                        }
+                    })
+                }
             }
-        }
+        })
     })
-})
 
 app.get('/kelolaDataBerita', redirectLogin, (req, res) => {
     dbConnection.con.query("SELECT * FROM dataBerita", (err, rows, field) => {
