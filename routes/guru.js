95b89aca0942f07f4a9e8bbae882e8a688dfb63f
Als <malkautsars@gmail.com>
2020-06-19 22:29:54 +0800

    feat: update profil guru

--- routes/guru.js
+++ routes/guru.js
@@ -3,7 +3,23 @@ const app = express()
 
 // Koneksi Database
 const dbConnection = require('../db_config/db_connection')
-let dataGuru;
+let idGuru;
+
+const redirectLogin = (req, res, next) => {
+    if (!req.session.userId) {
+        res.redirect('/guru')
+    } else {
+        next()
+    }
+}
+
+const redirectHome = (req, res, next) => {
+    if (req.session.userId) {
+        res.redirect('dashboard')
+    } else {
+        next()
+    }
+}
 
 const checkDataGuru = (email, pass) => {
     return new Promise((resolve, reject) => {
@@ -17,7 +33,7 @@ app.get('/', (req, res) => {
     res.render('guru/index')
 })
 
-app.post('/login', async (req, res) => {
+app.post('/login', redirectHome, async (req, res) => {
     const checkHasilDataGuru = await checkDataGuru(req.body.email, req.body.password)
 
     if (checkHasilDataGuru.length === 0) {
@@ -28,14 +44,82 @@ app.post('/login', async (req, res) => {
             password: ''
         })
     } else {
-        dataGuru = checkHasilDataGuru[0].id
+        req.session.userId = checkHasilDataGuru[0].id
+        idGuru = checkHasilDataGuru[0].id
         res.redirect('dashboard')
     }
 })
 
-app.get('/dashboard', (req, res) => {
-    console.log(dataGuru)
+app.route('/profil', redirectLogin)
+    .get((req, res) => {
+        dbConnection.con.query('SELECT * FROM dataGuru WHERE id = ?', [idGuru], (err, rows, fields) => {
+            let data = rows[0];
+            if (err) {
+                res.redirect('dashboard')
+            } else {
+                res.render('guru/profil', {
+                    id: data.id,
+                    namaLengkap: data.namaLengkap,
+                    tempatLahir: data.tempatLahir,
+                    tanggalLahir: data.tanggalLahir,
+                    jenisKelamin: data.jenisKelamin,
+                    agama: data.agama,
+                    alamat: data.alamat,
+                    nomorTelefon: data.nomorTelefon,
+                    email: data.email,
+                    password: data.password
+                })
+            }
+        })
+    })
+    .put((req, res) => {
+        let dataGuru = {
+            id: idGuru,
+            namaLengkap: req.sanitize('namaLengkap').escape().trim(),
+            tempatLahir: req.sanitize('tempatLahir').escape().trim(),
+            tanggalLahir: req.sanitize('tanggalLahir').escape().trim(),
+            alamat: req.sanitize('alamat').escape().trim(),
+            nomorTelefon: req.sanitize('nomorTelefon').escape().trim(),
+            jenisKelamin: req.sanitize('jenisKelamin').escape().trim(),
+            agama: req.sanitize('agama').escape().trim(),
+            email: req.sanitize('email').escape().trim(),
+            password: req.sanitize('password').escape().trim()
+
+        }
+        dbConnection.con.query("UPDATE dataGuru SET ? WHERE id = ?", [dataGuru, idGuru], (err, rows) => {
+            if (err) {
+                req.flash('error', err)
+                res.render('guru/profil', {
+                    id: idGuru,
+                    namaLengkap: dataGuru.namaLengkap,
+                    tempatLahir: dataGuru.tempatLahir,
+                    tanggalLahir: dataGuru.tanggalLahir,
+                    jenisKelamin: dataGuru.jenisKelamin,
+                    agama: dataGuru.agama,
+                    alamat: dataGuru.alamat,
+                    nomorTelefon: dataGuru.nomorTelefon,
+                    email: dataGuru.email,
+                    password: dataGuru.password
+                })
+            } else {
+                res.redirect('profil')
+            }
+        })
+    })
+
+app.get('/dashboard', redirectLogin, (req, res) => {
     res.render('guru/dashboard')
+})
+
+app.post('/logout', redirectLogin, (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            res.redirect('guru/dashboard')
+        } else {
+            res.clearCookie('sid')
+            res.redirect('/guru')
+        }
+    })
 })
 
 module.exports = app
